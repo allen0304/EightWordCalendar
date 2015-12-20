1d9e3226195c266966a526ef182ce05577d9c880
Allen <allen@emod.com.tw>
2015-12-20 21:16:24 +0800

    V1.0 Init

--- EightWordCalendar/ViewController.swift
+++ EightWordCalendar/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet var userView: UserView!
     @IBOutlet var date1: UILabel!
     @IBOutlet var date2: UILabel!
+    @IBOutlet var label節氣1: UILabel!
+    @IBOutlet var label節氣2: UILabel!
     let theDate = NSDate()
     
     
@@ -24,6 +26,12 @@ class ViewController: UIViewController {
         date2.text = ewCalendar.lunarDateString
         userView.setDate(theDate)
         
+        let fmt = NSDateFormatter()
+        fmt.calendar = gregorianCalendar
+        fmt.dateFormat = "M月d日 HH:mm"
+        label節氣1.text = ewCalendar.solarTerms[0].termName + "：" + fmt.stringFromDate(ewCalendar.solarTerms[0].termDate)
+        label節氣2.text = ewCalendar.solarTerms[1].termName + "：" + fmt.stringFromDate(ewCalendar.solarTerms[1].termDate)
+
     }
 
     override func didReceiveMemoryWarning() {
