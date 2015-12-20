d5bc09dda62d118f904bf35f4c5b8a6dc042941a
Allen <allen@emod.com.tw>
2015-12-20 20:17:33 +0800

    Initial Commit

---
+++ EightWordCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EightWordCalendar
+//
+//  Created by Allen on 2015/12/20.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
